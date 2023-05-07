f437c1ffba3a3336d4277b1fc3ef8795f9e0ed97
Carlos Santos <cast0277@gmail.com>
2023-05-07 14:32:38 -0600

    add-ref

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   }
 
   return (
-    <header className='flex justify-between py-4 px-2 text-2xl text-black items-center md:px-4 md:py-0 fixed top-0 left-0 w-full z-30 '>
+    <header className='flex justify-between py-4 px-2 text-2xl text-black items-center md:px-4 md:py-0 fixed top-0 left-0 w-full z-30 bg-slate-300 '>
       <div className='flex gap-5'>
         <img className='absolute left-4 top-2 md:left-[25px] md:top-[8px]' src="/images/elpY.png" alt="" />
         <img className='absolute md:top-[20px]' src="/images/EllipseX.png" alt="" />
@@ -20,17 +20,17 @@ const Navbar = () => {
 
       <nav className={`text-black absolute top-full ${isShowNav ? "left-0" : "-left-full"} w-full grid text-lg gap-2 duration-200 md:static md:flex md:w-auto md:mx-auto`}>
         <span className='hover:bg-purple-700/80 flex hover:gap-3 hover:text-white hover:rounded-md p-4 duration-200 pl-2'>
-          {'<'}<a href="#">Home</a>{'/>'}
+          {'<'}<a href="#"  onClick={handleShowNav}>Home</a>{'/>'}
         </span>
         <span className='hover:bg-purple-700/80 flex hover:gap-3 hover:text-white hover:rounded-md p-4 duration-200 pl-2'>
-          {'<'}<a href="#project">Project</a>{'/>'}
+          {'<'}<a href="#project" onClick={handleShowNav}>Project</a>{'/>'}
         </span>
         <span className='hover:bg-purple-700/80 flex hover:gap-3 hover:text-white hover:rounded-md p-4 duration-200 pl-2'>
-          {'<'}<a href="#">About me</a>{'/>'}
+          {'<'}<a href="#about_me"  onClick={handleShowNav}>About me</a>{'/>'}
         </span>
 
         <span className='hover:bg-purple-700/80 flex hover:gap-3 hover:text-white hover:rounded-md p-4 duration-200 pl-2'>
-          {'<'}<a href="#">Contact me</a>{'/>'}
+          {'<'}<a href="#footer" onClick={handleShowNav}>Contact me</a>{'/>'}
         </span>
  
       </nav>
