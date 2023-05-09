fde79c765b890d4fbf9e3e7985615f0c15981207
Carlos Santos <cast0277@gmail.com>
2023-05-08 22:30:58 -0600

    add-backgound-to-navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
         <img className='animate-bounce3' src="/images/robo.png" alt="" />
       </div>
 
-      <section className='max-w-[500px] md:ml-[5%] mx-auto px-1'>
-        <h2 id='about_me' className='text-Violet1 font-bold text-[28px] lg:text-5xl mb-7'>Hi! <span className='text-slate-700'>I’am a backend developed.</span></h2>
+      <section id='about_me' className='max-w-[500px] md:ml-[5%] mx-auto px-1'>
+        <h2 className='text-Violet1 font-bold text-[28px] lg:text-5xl mb-7'>Hi! <span className='text-slate-700'>I’am a backend developed.</span></h2>
         <p className=' text-slate-700 text-sm md:text-lg text-justify'>I’m probably the most passionate developer you will ever get to work with. If you have a great project that needs some amazing skills, I’m your guy.</p>
       </section>
     </section>
