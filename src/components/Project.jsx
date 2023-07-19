47c4f084e3193f6ab72c45ab3e41c87455d33018
Carlos Santos <cast0277@gmail.com>
2023-07-18 22:30:49 -0600

    change-language-in-project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,14 +6,14 @@ const Project = () => {
             {/* <h1 className='text-center text-slate-700 font-bold text-3xl sm:text-6xl pt-24 mb-10'>Project Portfolio</h1> */}
 
             <span className='text-Violet1 hover:text-violet-600 hover:font-bold hover:gap-2 sm:hover:gap-5 mt-20 mb-10 flex items-center justify-center text-2xl md:text-6xl duration-300'>{"<"}
-                <h1 className='text-center text-slate-700 font-bold text-[30px] md:text-6xl'>Project Portfolio</h1>{"/>"}
+                <h1 className='text-center text-slate-700 font-bold text-[30px] md:text-6xl'>Mis Proyectos</h1>{"/>"}
             </span>
             
             {/* ecommerce */}
             <section className='mx-auto sm:items-center sm:gap-12 sm:flex-row-reverse flex flex-col gap-5 px-3 py-[20px] sm:max-w-[1280px] sm:px-8 '>
                 <div className='ml-3 mb-5 sm:max-w-[45%] sm:mr-[6%]'>
-                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>E-commerce <span className='text-slate-700 group-hover:text-violet-600'>and some more projects.</span></h3>
-                    <span className='md:text-lg text-slate-700'>Interactive e-commerce using HTML, CSS and JavaScript. {<br />} Style and quality... you find everything here in your new favorite store.</span>
+                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>E-commerce <span className='text-slate-700 group-hover:text-violet-600'>y algunos proyectos más.</span></h3>
+                    <span className='md:text-lg text-slate-700'>Ecommerce interactivo utilizando HTML, CSS y JavaScript.. {<br />} Estilo y calidad... todo lo encuentras aquí en tu nueva tienda favorita..</span>
                     <div className='flex gap-3 text-slate-600 pt-2'>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>HTML</span>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>CSS</span>
@@ -25,7 +25,7 @@ const Project = () => {
                     <div className='relative overflow-hidden'>
                         <img className=' max-h-[320px]' src="/images/e-commerce.png" alt="" />
                         <div id='ecommerce_project' className={`absolute h-full w-full bg-cover flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-500 rounded-3xl`}>
-                            <a className='hover:bg-black rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://academlo-shop-carlos-santos.netlify.app/">Visit project</a>
+                            <a className='hover:bg-black rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://academlo-shop-carlos-santos.netlify.app/">Visualizar proyecto</a>
                         </div>
                     </div>
                 </div>
@@ -34,8 +34,8 @@ const Project = () => {
             {/* infoGalax */}
             <section className='mx-auto sm:items-center sm:gap-12 sm:flex-row flex flex-col gap-5 px-3 py-[20px] sm:max-w-[1280px] sm:px-8 '>
                 <div className='ml-3 sm:ml-[10%] mb-5 sm:max-w-[45%]'>
-                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>Infogalax<span className='text-slate-700 group-hover:text-violet-600'> knowing the universe.</span></h3>
-                    <span className='md:text-lg text-slate-700'>Entertainment page that shows random phrases about curiosities in space, using React with Vitejs, use of components, props and states.</span>
+                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>Infogalax<span className='text-slate-700 group-hover:text-violet-600'> Conociendo el universo.</span></h3>
+                    <span className='md:text-lg text-slate-700'>Página de entretenimiento que muestra frases aleatorias sobre curiosidades en el espacio, usando React con Vitejs, uso de componentes, props y estados.</span>
                     <div className='flex gap-3 text-slate-600 pt-2'>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>React</span>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>Vitejs</span>
@@ -45,7 +45,7 @@ const Project = () => {
                     <div className='relative overflow-hidden'>
                         <img className=' max-h-[320px]' src="/images/infoGalax.png" alt="" />
                         <div id='infoGalax' className='absolute h-full w-full bg-cover flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-500 rounded-3xl'>
-                            <a className='hover:bg-black rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://entregable01-carlos-alberto-santos.netlify.app/">Visit project</a>
+                            <a className='hover:bg-black rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://entregable01-carlos-alberto-santos.netlify.app/">Visualizar proyecto</a>
                         </div>
                     </div>
                 </div>
@@ -54,10 +54,8 @@ const Project = () => {
             {/* CRUD de usuarios */}
             <section className='mx-auto sm:items-center sm:gap-12 sm:flex-row-reverse flex flex-col gap-5 px-3 py-[20px] sm:max-w-[1280px] sm:px-8 '>
                 <div className='ml-3 mb-5 sm:max-w-[45%] sm:mr-[6%]'>
-                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>CRUD’s <span className='text-slate-700 group-hover:text-violet-600'>and HTTP methods.</span></h3>
-                    <span className='md:text-lg text-slate-700'>Application that allows you to manage users through a CRUD.
-                        Using API to query, create, delete and
-                        update users.</span>
+                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>CRUD’s <span className='text-slate-700 group-hover:text-violet-600'>y métodos HTTP.</span></h3>
+                    <span className='md:text-lg text-slate-700'>Aplicación que permite gestionar usuarios a través de un CRUD. Uso de API para consultar, crear, eliminar y actualizar usuarios.</span>
                     <div className='flex gap-3 text-slate-600 pt-2'>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>Tailwind</span>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>React</span>
@@ -69,7 +67,7 @@ const Project = () => {
                     <div className='relative overflow-hidden'>
                         <img className=' max-h-[320px]' src="/images/crudUsers.png" alt="" />
                         <div id='crudUsers' className='absolute h-full w-full bg-cover flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-500 rounded-3xl'>
-                            <a className='hover:bg-slate-500 rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://crud-entregable04.netlify.app/">Visit project</a>
+                            <a className='hover:bg-slate-500 rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://crud-entregable04.netlify.app/">Visualizar proyecto</a>
                         </div>
                     </div>
                 </div>
@@ -78,8 +76,8 @@ const Project = () => {
             {/* Pokedex */}
             <section className='mx-auto sm:items-center sm:gap-12 sm:flex-row flex flex-col gap-5 px-3 py-[20px] sm:max-w-[1280px] sm:px-8 '>
                 <div className='ml-3 sm:ml-[10%] mb-5 sm:max-w-[45%]'>
-                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>Pokedex<span className='text-slate-700 group-hover:text-violet-600'> with React router and Redux.</span></h3>
-                    <span className='md:text-lg text-slate-700'>Application that lists the pokemons brought from the pokeapi. It has a select to filter by type, number of pokemons to show per page and the option to search for a pokemon by name</span>
+                    <h3 className='text-violet-600 font-bold text-3xl lg:text-5xl mb-7 group hover:text-slate-700 duration-500'>Pokedex<span className='text-slate-700 group-hover:text-violet-600'> con React router y Redux.</span></h3>
+                    <span className='md:text-lg text-slate-700'>Aplicación que lista los pokemons traídos de la pokeapi. Tiene una selección para filtrar por tipo, número de pokemons a mostrar por página y la opción de buscar un pokemon por nombre.</span>
                     <div className='flex gap-3 text-slate-600 pt-2'>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>React</span>
                         <span className='border-[1px] rounded-sm border-Violet1 px-1 hover:text-violet-600 hover:border-slate-700'>Talwind</span>
@@ -91,7 +89,7 @@ const Project = () => {
                     <div className='relative overflow-hidden'>
                         <img className=' max-h-[320px]' src="/images/pokedex.png" alt="" />
                         <div id='pokedex' className='absolute h-full w-full bg-cover flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-500 rounded-3xl'>
-                            <a className='hover:bg-black rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://pokedex-carlos-santos.netlify.app">Visit project</a>
+                            <a className='hover:bg-black rounded-md bg-violet-400 text-white py-2 px-5' target='_blank' href="https://pokedex-carlos-santos.netlify.app">Visualizar proyecto</a>
                         </div>
                     </div>
                 </div>
