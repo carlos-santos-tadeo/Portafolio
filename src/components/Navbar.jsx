bd65145b4e2c48a82a6e9dc2475c6ad46e2e7e80
Carlos Santos <cast0277@gmail.com>
2023-07-20 18:00:41 -0600

    change-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="flex justify-between py-4 px-2 text-2xl text-black items-center md:px-4 md:py-0 fixed top-0 left-0 w-full z-30 bg-slate-300 ">
+    <header className="flex justify-between py-4 px-2 text-2xl text-black items-center lg:px-4 lg:py-0 fixed top-0 left-0 w-full z-30 bg-slate-300 ">
 
       <div>
         <img
@@ -20,36 +20,36 @@ const Navbar = () => {
 
       <i
         onClick={handleShowNav}
-        className="bx bx-menu text-black text-3xl cursor-pointer md:hidden hover:bg-purple-700/80 hover:text-white hover:rounded-md"
+        className="bx bx-menu text-black text 2xl font-semibold cursor-pointer lg:hidden hover:bg-purple-700/80 hover:text-white hover:rounded-lg"
       ></i>
 
       <nav
         className={`bg-slate-300/95 text-black absolute top-full ${
           isShowNav ? "left-0" : "-left-full"
-        } w-full grid text-lg lg:gap-2 duration-500 md:text-xl md:font-bold md:static md:flex md:w-auto lg:mr-16`}
+        } w-full grid text-xl font-semibold lg:gap-2 duration-500 lg:text-xl lg:font-bold lg:static lg:flex lg:w-auto lg:mr-16`}
       >
-        <span className="hover:bg-violet-600 flex hover:text-white hover:rounded-md p-4 duration-200 pl-2">
+        <span className="hover:bg-violet-600 flex justify-center hover:text-white hover:rounded-md p-4 duration-200 pl-2">
           {"<"}
           <a href="#" onClick={handleShowNav}>
             INICIO
           </a>
           {"/>"}
         </span>
-        <span className="hover:bg-violet-600 flex hover:text-white hover:rounded-md p-4 duration-200 pl-2">
+        <span className="hover:bg-violet-600 flex justify-center hover:text-white hover:rounded-md p-4 duration-200 pl-2">
           {"<"}
           <a href="#about_me" onClick={handleShowNav}>
             SOBRE MI
           </a>
           {"/>"}
         </span>
-        <span className="hover:bg-violet-600 flex hover:text-white hover:rounded-md p-4 duration-200 pl-2">
+        <span className="hover:bg-violet-600 flex justify-center hover:text-white hover:rounded-md p-4 duration-200 pl-2">
           {"<"}
           <a href="#project" onClick={handleShowNav}>
             PROYECTOS
           </a>
           {"/>"}
         </span>
-        <span className="hover:bg-violet-600 flex hover:text-white hover:rounded-md p-4 duration-200 pl-2">
+        <span className="hover:bg-violet-600 flex justify-center hover:text-white hover:rounded-md p-4 duration-200 pl-2">
           {"<"}
           <a href="#footer" onClick={handleShowNav}>
             CONTACTO
